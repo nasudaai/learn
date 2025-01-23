c8064c3353c78578cea7e9fdb07a8f18412bff86
nasuda <nasuda908@gmail.com>
2025-01-23 10:14:02 +0900

    add err

---
+++ day-dir/january-22/func-err.js
@@ -0,0 +1,10 @@
+const args = process.argv
+
+try {
+  console.log('try')
+
+  unFunc()
+} catch (err) {
+  console.log(err.message)
+}
+
--- sushi/sushi.sh
+++ sushi/sushi.sh
@@ -4,3 +4,5 @@ echo test
 
 echo $1 $2 $3 $4 >> yen.txt
 cat yen.txt
+
+node wpm.js $5 $2 $3
