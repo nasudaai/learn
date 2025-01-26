580542f745145178365c9b94e5e057852c657236
nasuda <nasuda908@gmail.com>
2025-01-26 13:18:42 +0900

    arr in obj code and forEach next>>

--- node-js/destructuring-assignment/arr.js
+++ node-js/destructuring-assignment/arr.js
@@ -1,6 +1,19 @@
-let a, b, rest
+let a, b, rest; 
 
-[a, b] = [1, 2]
+[a, b] = [1, 2];
+console.log(a, b);
+
+[a, b, ...rest] = [{name: "a"}, {name: "b"}, {name: "c"}, {name: "d"}, {name: "e"}]
 
 
-console.log(a, b)
+console.log(a)
+console.log(b)
+console.log(rest)
+
+
+rest.forEach((name) => {
+  
+  console.log(name.name)
+})
+
+
