2c620fa004669ba1d91beb90e33604aac9688a0d
nasuda <nasuda908@gmail.com>
2025-01-26 22:09:11 +0900

    add string loop

---
+++ node-js/string/str-like-arr.js
@@ -0,0 +1,13 @@
+const str = "s t r i n g"
+
+console.log(str[0])
+console.log(str[1])
+console.log(str[2])
+
+
+const len = str.length
+console.log(len)
+
+for (let i = 0; i < str.length; i += 1) {
+  console.log(str[i])
+}
