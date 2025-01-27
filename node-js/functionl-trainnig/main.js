95f6560c3d1e887416d35209dcde6ad463b8067d
nasuda <nasuda908@gmail.com>
2025-01-27 21:36:20 +0900

    add return fn

--- node-js/functionl-trainnig/main.js
+++ node-js/functionl-trainnig/main.js
@@ -1,3 +1,15 @@
-fn() {
-  console.log("main")
+function rWord() {
+  return "aaa"
 }
+
+const a = rWord()
+console.log(a)
+
+
+function use(callback) {
+  const a2 = callback()
+  console.log(a2)
+}
+
+
+use(rWord)
