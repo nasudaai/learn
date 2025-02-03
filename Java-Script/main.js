f6a567e9b72dae7e9b1e4d95f4631e5b42a1fa48
nasuda <nasuda908@gmail.com>
2025-02-04 00:33:26 +0900

    add if and Number.isNan

--- Java-Script/main.js
+++ Java-Script/main.js
@@ -1,3 +1,19 @@
-if (true) {
+const cond = true
+
+const qNum = Number.isNaN("string")
+
+if (cond) {
   console.log("main")
 }
+
+
+function isNNum (qNUm) {
+  if (qNum) {
+    console.log("Number")
+  } else {
+    console.log("Not a Number")
+  }
+}
+
+
+isNNum(qNum)
